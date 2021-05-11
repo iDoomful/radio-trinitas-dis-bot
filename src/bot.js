e933192a7e7a71bb71c82ce5e673e0e3356c7544
iDoomfull <liviucocianu@yahoo.com>
2021-05-11 16:20:46 +0300

    Live link updated

--- src/bot.js
+++ src/bot.js
@@ -6,6 +6,7 @@ const ytdl = require('ytdl-core');
 const client = new Discord.Client();
 const prefix = "+"
 
+var liveLink = "https://www.youtube.com/watch?v=En0YG4sRiL8"
 var servers = {}
 
 client.on('ready', () => {
@@ -42,7 +43,7 @@ client.on('ready', () => {
                                 servers[message.guild.id].resume(true);
                             } else {
                                 voiceChannel.join().then(connection => {
-                                    servers[message.guild.id] = connection.play(ytdl("https://www.youtube.com/watch?v=yGBBmNaW-HE&ab_channel=TRINITASTV"))
+                                    servers[message.guild.id] = connection.play(ytdl(liveLink))
                                         .on("finish", () => connection.disconnect())
                                 });
                             }
@@ -51,7 +52,7 @@ client.on('ready', () => {
                             console.log(`* ${message.member.displayName} issued 'trinitas join'`);
 
                             voiceChannel.join().then(connection => {
-                                servers[message.guild.id] = connection.play(ytdl("https://www.youtube.com/watch?v=yGBBmNaW-HE&ab_channel=TRINITASTV"))
+                                servers[message.guild.id] = connection.play(ytdl(liveLink))
                                     .on("finish", () => connection.disconnect())
                             });
 
@@ -66,6 +67,18 @@ client.on('ready', () => {
                             console.log(`* ${message.member.displayName} issued 'trinitas pause'`);
                             if (servers[message.guild.id]) servers[message.guild.id].pause(true);
                             break;
+                        case "updatelivelink":
+                            console.log(`* ${message.member.displayName} issued 'trinitas updatelivelink'`);
+
+                            if(message.member.user.tag === "3916" && message.member.displayName == "doomful" && args.length == 2) {
+                                liveLink = args[1];
+                                message.channel.send("Link-ul pentru live a fost actualizat!");
+                            } else {
+                                message.channel.send("Nu poti executa aceasta comanda");
+                            }
+                            break;
+                         case "invite":
+                            message.channel.send("Invita-l pe Dumnezeu in casa ta: https://discord.com/oauth2/authorize?client_id=786306857249407086&scope=bot&permissions=36700424");
                     }
                 } else {
                     message.channel.send("Nu ai permisiunea sa executi aceste comenzi");
